d4c0a5813096a70a2c846d82c7f0d13b12e0ee45
draculola1 <draculola1@bwg-io.site>
2023-05-22 12:42:19 +0300

    iter

--- src/features/header/header.tsx
+++ src/features/header/header.tsx
@@ -62,7 +62,7 @@ const Header: FC<IHeaderProps> = () => {
                     <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="mobile-menu-2">
                         <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                             <li>
-                                <Link href="/dash" className={navLinkClass} aria-current="page">
+                                <Link href="/dashboard" className={navLinkClass} aria-current="page">
                                     Главная
                                 </Link>
                             </li>
